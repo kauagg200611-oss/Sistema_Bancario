5c3ac68df38e430a2f61358c2ffa0140954920e1
Kauã Germano <kauagg200611@gmail.com>
2026-08-01 19:59:20 -0300

    Adiciona Sistema-Bancario ao repositório

---
+++ Sistema_Bancario/Banco.java
@@ -0,0 +1,19 @@
+package Sistema_Bancario;
+
+public class Banco {
+    private int numeroBanco;   
+    private Cliente[] clientes;
+    private Conta[] contas;
+
+    /* 1 - O código deve verificar se ainda existe vetores vazios para q uma conta nova possa ocupar aqle lugar;
+    2 - Usar ArrayList */
+    public Banco(int numeroBanco, int capacidadeClientes, int capacidadeContas) {
+        this.numeroBanco = numeroBanco;
+        this.clientes = new Cliente[capacidadeClientes];
+        this.contas = new Conta[capacidadeContas];
+    }
+
+    public int getNumeroBanco(){
+        return numeroBanco;
+    }
+}
---
+++ Sistema_Bancario/Conta.java
@@ -0,0 +1,46 @@
+package Sistema_Bancario;
+
+public class Conta {
+    private String numeroConta;
+    private double saldo;
+    private Cliente titular;
+
+    public Conta(String numeroConta, double saldo, Cliente titular){
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+        this.titular = titular;
+    }
+
+    public String getNumeroConta(){
+        return numeroConta;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public Cliente getTitular(){
+        return titular;
+    }
+
+    public void depositar(double valor){
+        if(valor <= 0){
+            return;
+        }
+        saldo += valor;
+    }
+
+    public boolean sacar(double valor){
+        if(valor <= 0 || valor > saldo){
+            return false;
+        }
+        saldo -= valor;
+        return true;
+    }
+
+    public void transferir(double valor, Conta destino){
+        if(sacar(valor)){
+            destino.depositar(valor);
+        }
+    }
+}
