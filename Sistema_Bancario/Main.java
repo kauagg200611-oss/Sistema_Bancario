5ce24c4706e4834df03d8c886a247c4cceef3e67
Kauã Germano <kauagg200611@gmail.com>
2026-09-01 23:07:20 -0300

    Testando uma terceira conta

--- Sistema_Bancario/Main.java
+++ Sistema_Bancario/Main.java
@@ -6,26 +6,33 @@ public class Main {
 
         Cliente cliente1 = new Cliente("Paulo", "12345678901", 37);
         Cliente cliente2 = new Cliente("Maria", "09876543210", 28);
+        Cliente cliente3 = new Cliente("Kauã", "23412356489", 20);
 
         banco.adicionarCliente(cliente1);
         banco.adicionarCliente(cliente2);
+        banco.adicionarCliente(cliente3);
 
         Conta conta1 = new Conta("001", cliente1);
         Conta conta2 = new Conta("002", cliente2);
+        Conta conta3 = new Conta("003", cliente3);
 
-        banco.adicionarConta(conta1);
+        banco.adicionarConta(conta1); 
         banco.adicionarConta(conta2);
+        banco.adicionarConta(conta3);
 
         banco.procurarConta("001");
+        banco.procurarConta("002");
         banco.procurarConta("003");
 
         banco.listarContas();  
 
         conta2.depositar(1000);
         conta1.depositar(1000);
+        conta3.depositar(9000);
         
         System.out.println("saldo da conta 1: " + conta1.getSaldo());
         System.out.println("saldo da conta 2: " + conta2.getSaldo());
+        System.out.println("saldo da conta 3: " + conta3.getSaldo());
 
         conta1.transferir(2000, conta2);
         System.out.println("saldo da conta 1 após tentativa de transferência: " + conta1.getSaldo());
